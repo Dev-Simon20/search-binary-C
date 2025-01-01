86924ee2a6cfb118e31264c8409db94295035c21
Dev-Simon20 <jeanpierks6@gmail.com>
2025-01-01 18:12:40 -0500

    "prueba de algoritmo de busqueda binaria"

--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
-﻿Console.WriteLine("Hola  que tal, !");
+﻿
+int[] numeros = [1, 2, 3, 4, 5, 6, 7, 8,9]; 
+static int? busquedabinaria(int[] lista,int elemento) {
 
-Console.WriteLine("Modificando la segunda linea");
+    int menor = 0;
+    int mayor=lista.Length-1;
+
+    while (menor<=mayor) {
+        int medio = (menor + mayor) / 2;
+        int estimado = lista[medio];
+        if (estimado == elemento) {
+            return medio;
+         }
+        if (estimado > elemento) {
+            mayor = medio - 1;
+        }
+        else {
+            menor = medio + 1;
+        } 
+    }
+
+    return null;
+
+}
+
+Console.WriteLine(busquedabinaria(numeros, 10));
+
